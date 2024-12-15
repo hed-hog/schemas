20d8a5f3bf7eceed806552015eb4d4478396d407
saulotarsobc <saulotarsobc@gmail.com>
2024-12-15 18:59:34 -0300

    required true

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -6,7 +6,7 @@ export const INTERFACES_PATH = glob.sync('./src/interfaces/**/*.ts');
 export const SCHEMA_DIR = resolve(__dirname, '..', '..', 'schemas');
 
 export const SETTINGS: PartialArgs = {
-  required: false,
+  required: true,
   aliasRef: false,
   titles: true,
   noExtraProps: true,
@@ -15,4 +15,5 @@ export const SETTINGS: PartialArgs = {
 export const COMPILER_OPTIONS: CompilerOptions = {
   strictNullChecks: true,
   additionalProperties: true,
+  strict: true,
 };
